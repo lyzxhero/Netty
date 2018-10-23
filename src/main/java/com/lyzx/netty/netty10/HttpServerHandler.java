b48e05d8283c5587b6946d1e652a85f5ddd1c1c7
hero.li <liyaohui@juxiangfen.com>
2018-10-23 14:42:20 +0800

    netty开发Http协议应用

--- src/main/java/com/lyzx/netty/netty10/HttpServerHandler.java
+++ src/main/java/com/lyzx/netty/netty10/HttpServerHandler.java
@@ -27,7 +27,7 @@ public class HttpServerHandler extends SimpleChannelInboundHandler<FullHttpReque
     @Override
     public void channelRead0(ChannelHandlerContext ctx,FullHttpRequest request) throws Exception {
         // 首先检查解码是否成功，如果失败则返回错误页面
-        if (!request.decoderResult().isSuccess()) {
+        if (!request.decoderResult().isSuccess()){
             sendError(ctx,HttpResponseStatus.BAD_REQUEST);
             return;
         }
